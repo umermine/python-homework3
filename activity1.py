00f2cada06024d18c6a8a5e321fff83d0ef8e2d9
umermine <umermine4@gmail.com>
2024-12-11 17:49:45 +0500

    homework 3

---
+++ activity1.py
@@ -0,0 +1,7 @@
+greeting = "Hello, "
+name = input("Whats your name ")
+full_greeting = greeting + name + "!"
+print(full_greeting)
+var = input("Write a statement ")
+varfixed = name + " says " + var
+print(varfixed) 
